a4301168b60d246a816f3e0f198f9b58f9c11314
Oleandret <oleandret@icloud.com>
2024-12-04 17:50:29 +0100

    Update __init__.py

--- functions/__init__.py
+++ functions/__init__.py
@@ -2,22 +2,18 @@ from pathlib import Path
 import importlib
 import inspect
 import logging
-from typing import Dict, Type
+from typing import Dict
 from .function_base import BaseFunction
+from .homey.lights import HomeyLights
 
 logger = logging.getLogger(__name__)
 
-def get_all_functions() -> Dict[str, Type[BaseFunction]]:
+def get_all_functions() -> Dict[str, BaseFunction]:
     functions = {}
-    current_dir = Path(__file__).parent
-
-    # Last homey/lights.py direkte
     try:
-        from .homey.lights import HomeyLights
         instance = HomeyLights()
         functions[instance.name] = instance
         logger.info(f"Lastet funksjon: {instance.name}")
     except Exception as e:
         logger.error(f"Feil ved lasting av HomeyLights: {e}")
-
     return functions
