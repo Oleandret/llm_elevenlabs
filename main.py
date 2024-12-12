176d07677ffcc19e2fc71f88a7b7d4c9af0c0008
Oleandret <oleandret@icloud.com>
2024-12-12 19:16:49 +0100

    Update main.py

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ load_dotenv()
 # Sett opp logging
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(name=s - %(levelname)s - %(message)s'
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'
 )
 logger = logging.getLogger(__name__)
 
